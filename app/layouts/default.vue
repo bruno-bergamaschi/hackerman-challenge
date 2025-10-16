50cb111e727f73b5ca5f6934376e5578d54ead55
Bruno Bergamaschi <brunobergamaschi@live.com>
2025-10-15 21:50:36 -0300

    feat: implement layout default

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  min-height: 100dvh;
+  background: url("~/assets/img/StarWarsBackground.png") no-repeat center/cover;
+}
+</style>
