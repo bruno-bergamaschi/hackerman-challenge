17e0a005857b15b22ef27fce8200921d5aaa2fc6
Bruno Bergamaschi <brunobergamaschi@live.com>
2025-10-15 21:43:42 -0300

    feat: implement loading component

---
+++ app/components/ui/loading/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Card>
+    <div class="loading">
+      <div class="loading__spinner" />
+      <p class="loading__text">Carregando...</p>
+    </div>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Card from "~/components/ui/card/index.vue";
+</script>
+
+<style scoped lang="scss" src="./style.scss" />
