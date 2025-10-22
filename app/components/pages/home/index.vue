5331652b5060bbc905b53a22a762f2e7953566a6
Bruno Bergamaschi <brunobergamaschi@live.com>
2025-10-22 10:10:24 -0300

    feat: implement components to home and search pages

---
+++ app/components/pages/home/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="content">
+    <div class="content__logo" />
+
+    <Card title="Olá!" subtitle="Para começar seu teste, digite um e-mail">
+      <div class="content__input">
+        <Input
+          v-model="email"
+          placeholder="exemplo@email.com"
+          :is-valid-value="useValidateEmail(email)"
+        />
+      </div>
+
+      <Button
+        text="Próximo"
+        :disabled="!useValidateEmail(email)"
+        @click="navigateToSearchPage"
+      />
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useUserEmailStore } from "~/stores/userEmail";
+import { useValidateEmail } from "~/composables/useValidateEmail";
+
+import Card from "~/components/ui/card/index.vue";
+import Input from "~/components/ui/input/index.vue";
+import Button from "~/components/ui/button/index.vue";
+
+const { setEmail } = useUserEmailStore();
+
+const email = ref("");
+
+function navigateToSearchPage() {
+  setEmail(email.value);
+  navigateTo("/search");
+}
+</script>
+
+<style scoped lang="scss" src="./style.scss" />
--- app/pages/search/index.vue
+++ app/pages/search/index.vue
@@ -1,116 +1,11 @@
 <template>
-  <div class="search-content">
-    <Transition>
-      <Loading v-if="isLoading" class="position-absolute" />
-
-      <Card
-        v-else-if="!isLoading && !peopleData"
-        class="position-absolute"
-        subtitle="Agora, digite seu personagem favorito de Star Wars"
-      >
-        <div class="search-content__input">
-          <Input
-            v-model="inputValue"
-            :error-message="errorMessage"
-            :is-valid-value="isValidInputValue"
-          />
-        </div>
-
-        <Button text="Próximo" @click="fetchPeopleData" />
-      </Card>
-
-      <PeopleData
-        v-else-if="!isLoading && peopleData"
-        class="position-absolute"
-        :peopleData="peopleData"
-        @search:reset="resetSearch"
-      />
-    </Transition>
-  </div>
+ <Search/>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import type { ISwapiPerson } from "~/stores/swapiApi/people";
-import { useSwapiApiPeopleStore } from "~/stores/swapiApi/people";
-
-import Card from "~/components/ui/card/index.vue";
-import Input from "~/components/ui/input/index.vue";
-import Button from "~/components/ui/button/index.vue";
-import Loading from "~/components/ui/loading/index.vue";
-import PeopleData from "~/components/peopleData/index.vue";
-
-const {
-  fetchSwapiPeopleData,
-  getSwapiPeopleDataCache,
-  removeSwapiPeopleDataCache,
-} = useSwapiApiPeopleStore();
+import Search from '~/components/pages/search/index.vue'
 
 definePageMeta({
   middleware: "validate-email",
 });
-
-const inputValue = ref("");
-const isLoading = ref(true);
-const hasFetchError = ref(false);
-const peopleData = ref<ISwapiPerson | null>(null);
-
-const errorMessage = computed(() => {
-  return hasFetchError.value ? "Personagem inválido" : null;
-});
-
-onNuxtReady(() => {
-  const { people } = getSwapiPeopleDataCache();
-
-  if (people) {
-    peopleData.value = people;
-  }
-
-  isLoading.value = false;
-});
-
-watch(inputValue, () => {
-  if (hasFetchError.value) {
-    hasFetchError.value = false;
-  }
-});
-
-const isValidInputValue = computed(() => {
-  return !hasFetchError.value && inputValue.value.trim().length > 0;
-});
-
-async function fetchPeopleData() {
-  isLoading.value = true;
-  hasFetchError.value = false;
-
-  try {
-    if (!isValidInputValue.value) {
-      hasFetchError.value = true;
-      return;
-    }
-
-    const { count, people } = await fetchSwapiPeopleData({
-      search: inputValue.value,
-      useStorageData: false,
-    });
-
-    if (count === 0) {
-      hasFetchError.value = true;
-      peopleData.value = null;
-      return;
-    }
-
-    peopleData.value = people;
-  } finally {
-    isLoading.value = false;
-  }
-}
-
-function resetSearch() {
-  peopleData.value = null;
-  removeSwapiPeopleDataCache();
-  inputValue.value = "";
-}
 </script>
-
-<style scoped lang="scss" src="./style.scss" />
